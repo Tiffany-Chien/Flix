246c97b6881b90dc191997e76a9fb886a4c57554
Tiffany <tiffanychien9@gmail.com>
2021-10-01 21:01:06 -0400

    add final layout

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by ChienTiffany on 10/1/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+    
+    
+}
